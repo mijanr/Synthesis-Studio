51be9162ed68b84f106f8060e683539b5084741a
Md Mijanur Rahman <md.rahman@fau.de>
2023-11-21 22:17:48 +0100

    removed unnecessary imports

--- GANs/src/cGAN.py
+++ GANs/src/cGAN.py
@@ -9,11 +9,6 @@ cGAN.py
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
-
 
 #Generator: G(z, y)
 class Generator(nn.Module):

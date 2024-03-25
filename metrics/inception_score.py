67e5b4448bea7c06376f58cf2e70422047d37d28
Md Mijanur Rahman <md.rahman@fau.de>
2024-03-25 10:25:49 +0100

    Add inception_score.py for calculating the inception score

---
+++ metrics/inception_score.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def inception_score(preds, n_split=10, eps=1E-16):
+    # calculate the inception score
+    scores = []
+    n_part = preds.shape[0] // n_split
+    for i in range(n_split):
+        part = preds[(i * n_part):((i + 1) * n_part), :]
+        kl_d = part * (np.log(part + eps) - np.log(np.expand_dims(np.mean(part, 0), 0) + eps))
+        kl_d = np.mean(np.sum(kl_d, 1))
+        scores.append(np.exp(kl_d))
+    return np.mean(scores), np.std(scores)
+
+
+if __name__ == '__main__':
+    # test inception score
+    preds = np.random.rand(1000, 10)
+    print(inception_score(preds))
